b179515a545f1188e5175afbde95fe6095808adb
Senpaiyaa <ignacioerwin00@gmail.com>
2020-08-04 19:30:41 +0800

    the vue instance

---
+++ main.js
@@ -0,0 +1,17 @@
+// Setup vue instance 
+var app = new Vue({
+
+	/**
+	* Properties:
+	*
+	* el -> this property plugs intance in to the div with the id of app
+	* data -> give the instance a product whole value is Socks
+	*/
+
+	el: '#app',
+	data: {
+		product: 'Socks'
+		// product: 'Boots'
+	}
+
+})
